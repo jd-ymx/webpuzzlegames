a5bda1fa7394b166e69dad0b4154e81be8284385
David <rongweifang6@gmail.com>
2025-06-10 17:48:45 +0800

    测试版本4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   images: {
+    unoptimized: true,
     domains: ['play2048.co', 'tetris.com', 'www.google.com', 'www.chess.com', 'sudoku.com'],
     remotePatterns: [
       {
